25535eabc6f5ad1fdd98f6639d2ab50251d7b9f2
Maximilien Naveau <maximilien.naveau@gmail.com>
2023-06-29 16:44:57 +0200

    add a log message in case we detect some Nans

--- src/linear_feedback_controller.cpp
+++ src/linear_feedback_controller.cpp
@@ -458,6 +458,7 @@ void LinearFeedbackController::filterInitialState() {
   initial_torque_ = initial_torque_filter_.getFilteredData();
   for (Eigen::Index i = 0; i < initial_torque_.size(); ++i) {
     if (std::isnan(initial_torque_(i))) {
+      ROS_WARN_ONCE("Nan detected for joint " << getControlledJointNames()[i]);
       initial_torque_(i) = 0.0;
     }
   }
@@ -489,6 +490,8 @@ void LinearFeedbackController::acquireSensorAndPublish(
         getJointMeasuredTorque(pin_to_hwi_[i]) -
         in_torque_offsets_[pin_to_hwi_[i]];
     if (std::isnan(eigen_sensor_msg_.joint_state.effort(i))) {
+      ROS_WARN_ONCE("Nan detected for joint "
+                    << eigen_sensor_msg_.joint_state.name[i]);
       eigen_sensor_msg_.joint_state.effort(i) = 0.0;
     }
   }
