38d27c0166cfb10f2d2b3cf978a710a578b7da9e
Maximilien Naveau <maximilien.naveau@toward.fr>
2024-10-19 22:39:38 +0200

    fix some format from a code review

--- include/linear_feedback_controller/robot_model_builder.hpp
+++ include/linear_feedback_controller/robot_model_builder.hpp
@@ -19,33 +19,18 @@ class RobotModelBuilder {
   virtual ~RobotModelBuilder();
 
   /**
-   * @brief
+   * @brief Build the robot model freezing the joints that are not moving.
    *
-   * @param in_urdf Content of the urdf file.
-   * @param in_srdf Content of the srdf file.
-   * @param in_moving_joint_names List of active joint name.
-   * @param in_robot_has_free_flyer If the robot has a free flyer or not.
+   * @param urdf Content of the urdf file.
+   * @param srdf Content of the srdf file.
+   * @param moving_joint_names List of active joint name.
+   * @param controlled_joint_names List of actively controlled joints.
+   * @param robot_has_free_flyer If the robot has a free flyer or not.
    */
-  bool build_model(const std::string& in_urdf, const std::string& in_srdf,
-                   const std::vector<std::string>& in_moving_joint_names,
-                   bool in_robot_has_free_flyer,
-                   const std::vector<std::string>& in_controlled_joint_names);
-
-  /**
-   * @brief Parse the joint moving names given by the user and build the
-   * rigid body models accordingly.
-   *
-   * @param in_moving_joint_names
-   * @param moving_joint_names
-   * @param moving_joint_ids
-   * @param locked_joint_ids
-   */
-  bool parseMovingJointNames(
-      const std::vector<std::string>& in_moving_joint_names,
-      const std::vector<std::string>& in_controlled_joint_names,
-      std::vector<std::string>& moving_joint_names,
-      std::vector<pinocchio::Index>& moving_joint_ids,
-      std::vector<pinocchio::Index>& locked_joint_ids);
+  bool buildModel(const std::string& urdf, const std::string& srdf,
+                  const std::vector<std::string>& moving_joint_names,
+                  const std::vector<std::string>& controlled_joint_names,
+                  bool robot_has_free_flyer);
 
   /**
    * @brief Get the moving joint names.
@@ -76,6 +61,13 @@ class RobotModelBuilder {
   const std::string& getUrdf() const;
 
   /**
+   * @brief Get the SRDF string.
+   *
+   * @return const std::string&
+   */
+  const std::string& getSrdf() const;
+
+  /**
    * @brief Get the pinocchio model.
    *
    * @return const pinocchio::Model&
@@ -97,24 +89,29 @@ class RobotModelBuilder {
    */
   bool getRobotHasFreeFlyer();
 
+  /**
+   * @brief Get the Pinocchio To Harwdare Interface Map.
+   *
+   * @return std::map<int, int>
+   */
+  std::map<int, int> getPinocchioToHarwdareInterfaceMap();
+
  private:
   /// @brief String containing the model of the robot in xml/urdf format.
-  std::string in_urdf_;
+  std::string urdf_;
   /// @brief String containing the extras of the model of the robot.
-  std::string in_srdf_;
+  std::string srdf_;
   /// @brief List of names that correspond to the joints moving by the MPC.
-  std::vector<std::string> in_moving_joint_names_;
+  std::vector<std::string> moving_joint_names_;
   /// @brief Are we using a robot that has a free-flyer?
-  bool in_robot_has_free_flyer_;
+  bool robot_has_free_flyer_;
   /// @brief The ordered list of the joint names that are actually controlled
-  std::vector<std::string> in_controlled_joint_names_;
+  std::vector<std::string> controlled_joint_names_;
 
   /// @brief Mapping from the pinocchio indexing to the hardware interface.
   std::map<int, int> pin_to_hwi_;
   /// @brief Moving joint ids sorted in the urdf order.
   std::vector<pinocchio::JointIndex> moving_joint_ids_;
-  /// @brief Sort the moving joint names using the urdf order.
-  std::vector<std::string> moving_joint_names_;
   /// @brief Sort the locked (position moving) joint names using the urdf order.
   std::vector<pinocchio::JointIndex> locked_joint_ids_;
 
@@ -130,6 +127,21 @@ class RobotModelBuilder {
   /// @brief Initial whole body configuration setup in the SRDF file.
   Eigen::VectorXd q_default_complete_;
 
+ private:
+  /**
+   * @brief Parse the joint moving names given by the user and build the
+   * rigid body models accordingly.
+   *
+   * @param moving_joint_names
+   * @param moving_joint_names
+   * @param moving_joint_ids
+   * @param locked_joint_ids
+   */
+  bool parseMovingJointNames(
+      const std::vector<std::string>& moving_joint_names,
+      const std::vector<std::string>& controlled_joint_names);
+
+ public:
   using SharedPtr = std::shared_ptr<RobotModelBuilder>;
 };
 
--- src/robot_model_builder.cpp
+++ src/robot_model_builder.cpp
@@ -13,35 +13,34 @@ RobotModelBuilder::RobotModelBuilder() {}
 
 RobotModelBuilder::~RobotModelBuilder() {}
 
-bool RobotModelBuilder::build_model(
-    const std::string& in_urdf, const std::string& in_srdf,
-    const std::vector<std::string>& in_moving_joint_names,
-    bool in_robot_has_free_flyer,
-    const std::vector<std::string>& in_controlled_joint_names) {
+bool RobotModelBuilder::buildModel(
+    const std::string& urdf, const std::string& srdf,
+    const std::vector<std::string>& moving_joint_names,
+    const std::vector<std::string>& controlled_joint_names,
+    bool robot_has_free_flyer) {
   // Copy the argument inside the class.
-  in_urdf_ = in_urdf;
-  in_srdf_ = in_srdf;
-  in_moving_joint_names_ = in_moving_joint_names;
-  in_robot_has_free_flyer_ = in_robot_has_free_flyer;
-  in_controlled_joint_names_ = in_controlled_joint_names;
+  urdf_ = urdf;
+  srdf_ = srdf;
+  moving_joint_names_ = moving_joint_names;
+  robot_has_free_flyer_ = robot_has_free_flyer;
+  controlled_joint_names_ = controlled_joint_names;
 
   // Build the rigid body model of the robot.
-  if (in_robot_has_free_flyer_) {
-    pinocchio::urdf::buildModelFromXML(
-        in_urdf_, pinocchio::JointModelFreeFlyer(), pinocchio_model_complete_);
+  if (robot_has_free_flyer_) {
+    pinocchio::urdf::buildModelFromXML(urdf_, pinocchio::JointModelFreeFlyer(),
+                                       pinocchio_model_complete_);
   } else {
-    pinocchio::urdf::buildModelFromXML(in_urdf_, pinocchio_model_complete_);
+    pinocchio::urdf::buildModelFromXML(urdf_, pinocchio_model_complete_);
   }
-  std::istringstream iss_srdf(in_srdf_);
+  std::istringstream iss_srdf(srdf_);
   pinocchio::srdf::loadReferenceConfigurationsFromXML(pinocchio_model_complete_,
                                                       iss_srdf, false);
 
   // Reduce the rigid body model and set initial position.
-  if (!parseMovingJointNames(in_moving_joint_names_, in_controlled_joint_names_,
-                             moving_joint_names_, moving_joint_ids_,
-                             locked_joint_ids_)) {
+  if (!parseMovingJointNames(moving_joint_names_, controlled_joint_names_)) {
     return false;
   }
+
   q_default_complete_ =
       pinocchio_model_complete_.referenceConfigurations["half_sitting"];
   pinocchio_model_reduced_ = pinocchio::buildReducedModel(
@@ -52,94 +51,88 @@ bool RobotModelBuilder::build_model(
 }
 
 bool RobotModelBuilder::parseMovingJointNames(
-    const std::vector<std::string>& in_moving_joint_names,
-    const std::vector<std::string>& in_controlled_joint_names,
-    std::vector<std::string>& moving_joint_names,
-    std::vector<pinocchio::Index>& moving_joint_ids,
-    std::vector<pinocchio::Index>& locked_joint_ids) {
+    const std::vector<std::string>& moving_joint_names,
+    const std::vector<std::string>& controlled_joint_names) {
   // Get moving joints ids
-  moving_joint_ids.clear();
-  for (std::vector<std::string>::const_iterator it =
-           in_moving_joint_names.begin();
-       it != in_moving_joint_names.end(); ++it) {
-    const std::string& joint_name = *it;
+  moving_joint_ids_.clear();
+  for (const auto& joint_name : moving_joint_names) {
     pinocchio::JointIndex joint_id = 0;
     // do not consider joint that are not in the model
     if (pinocchio_model_complete_.existJointName(joint_name)) {
       joint_id = pinocchio_model_complete_.getJointId(joint_name);
-      moving_joint_ids.push_back(joint_id);
+      moving_joint_ids_.push_back(joint_id);
     } else {
       std::cerr << "joint_name=" << joint_name
                 << " does not belong to the model" << std::endl;
     }
   }
-  if (in_robot_has_free_flyer_) {
-    moving_joint_ids.push_back(
+  if (robot_has_free_flyer_) {
+    moving_joint_ids_.push_back(
         pinocchio_model_complete_.getJointId("root_joint"));
   }
   // Sort them to the pinocchio order (increasing number) and remove duplicates.
-  std::sort(moving_joint_ids.begin(), moving_joint_ids.end());
-  moving_joint_ids.erase(
-      unique(moving_joint_ids.begin(), moving_joint_ids.end()),
-      moving_joint_ids.end());
+  std::sort(moving_joint_ids_.begin(), moving_joint_ids_.end());
+  moving_joint_ids_.erase(
+      unique(moving_joint_ids_.begin(), moving_joint_ids_.end()),
+      moving_joint_ids_.end());
 
   // Remap the moving joint names to the Pinocchio order.
-  moving_joint_names.clear();
-  for (std::size_t i = 0; i < moving_joint_ids.size(); ++i) {
-    moving_joint_names.push_back(
-        pinocchio_model_complete_.names[moving_joint_ids[i]]);
+  moving_joint_names_.clear();
+  for (std::size_t i = 0; i < moving_joint_ids_.size(); ++i) {
+    moving_joint_names_.push_back(
+        pinocchio_model_complete_.names[moving_joint_ids_[i]]);
   }
 
   // Locked joint ids in the Pinocchio order.
-  locked_joint_ids.clear();
+  locked_joint_ids_.clear();
   for (std::vector<std::string>::const_iterator it =
            pinocchio_model_complete_.names.begin() + 1;
        it != pinocchio_model_complete_.names.end(); ++it) {
     const std::string& joint_name = *it;
-    if (std::find(moving_joint_names.begin(), moving_joint_names.end(),
-                  joint_name) == moving_joint_names.end()) {
-      locked_joint_ids.push_back(
+    if (std::find(moving_joint_names_.begin(), moving_joint_names_.end(),
+                  joint_name) == moving_joint_names_.end()) {
+      locked_joint_ids_.push_back(
           pinocchio_model_complete_.getJointId(joint_name));
     }
   }
 
-  // remove the "root_joint" if it exists in the moving_joint_names, we needed
+  // remove the "root_joint" if it exists in the moving_joint_names_, we needed
   // it to create the above locked_joint_ids needed for the reduced model.
-  auto root_name = std::remove(moving_joint_names.begin(),
-                               moving_joint_names.end(), "root_joint");
-  if (root_name != moving_joint_names.end()) {
-    moving_joint_names.erase(root_name, moving_joint_names.end());
+  auto root_name = std::remove(moving_joint_names_.begin(),
+                               moving_joint_names_.end(), "root_joint");
+  if (root_name != moving_joint_names_.end()) {
+    moving_joint_names_.erase(root_name, moving_joint_names_.end());
     auto root_id =
-        std::remove(moving_joint_ids.begin(), moving_joint_ids.end(),
+        std::remove(moving_joint_ids_.begin(), moving_joint_ids_.end(),
                     pinocchio_model_complete_.getJointId("root_joint"));
-    moving_joint_ids.erase(root_id, moving_joint_ids.end());
+    moving_joint_ids_.erase(root_id, moving_joint_ids_.end());
   }
 
   // Create the joint name joint id mapping of the hardware interface.
-  for (std::size_t i = 0; i < moving_joint_names.size(); ++i) {
-    auto it = std::find(in_controlled_joint_names.begin(),
-                        in_controlled_joint_names.end(), moving_joint_names[i]);
+  for (std::size_t i = 0; i < moving_joint_names_.size(); ++i) {
+    auto it = std::find(controlled_joint_names.begin(),
+                        controlled_joint_names.end(), moving_joint_names_[i]);
     // verify that the found moving joints are part of the hardware interface.
-    if (it == in_controlled_joint_names.end()) {
+    if (it == controlled_joint_names.end()) {
       std::cerr << "LinearFeedbackController::parseMovingJointNames(): "
-                << "Moving joint " << moving_joint_names[i]
+                << "Moving joint " << moving_joint_names_[i]
                 << " is not part of the current roscontrol hardware "
                 << "interface." << std::endl;
       return false;
     }
     std::size_t it_dist =
-        (size_t)std::distance(in_controlled_joint_names.begin(), it);
+        (size_t)std::distance(controlled_joint_names.begin(), it);
     pin_to_hwi_.emplace(std::make_pair(i, it_dist));
   }
   // Double check what we just did
-  for (std::size_t i = 0; i < moving_joint_names.size(); ++i) {
-    if (moving_joint_names[i] != in_controlled_joint_names[pin_to_hwi_[i]]) {
+  for (std::size_t i = 0; i < moving_joint_names_.size(); ++i) {
+    if (moving_joint_names_[i] != controlled_joint_names[pin_to_hwi_[i]]) {
       std::cerr << "LinearFeedbackController::parseMovingJointNames(): "
                 << "The mapping from pinocchio to roscontrol is wrong. "
-                << "moving_joint_names[i] != "
-                << "in_controlled_joint_names[pin_to_hwi_[i]] => "
-                << moving_joint_names[i]
-                << " != " << in_controlled_joint_names[pin_to_hwi_[i]];
+                << "moving_joint_names_[i] != "
+                << "controlled_joint_names[pin_to_hwi_[i]] => "
+                << moving_joint_names_[i]
+                << " != " << controlled_joint_names[pin_to_hwi_[i]];
       return false;
     }
   }
@@ -160,7 +153,9 @@ const std::vector<long unsigned int>& RobotModelBuilder::getLockedJointIds()
   return locked_joint_ids_;
 }
 
-const std::string& RobotModelBuilder::getUrdf() const { return in_urdf_; }
+const std::string& RobotModelBuilder::getUrdf() const { return urdf_; }
+
+const std::string& RobotModelBuilder::getSrdf() const { return srdf_; }
 
 const pinocchio::Model& RobotModelBuilder::getPinocchioModel() const {
   return pinocchio_model_reduced_;
@@ -170,8 +165,10 @@ pinocchio::Data& RobotModelBuilder::getPinocchioData() {
   return pinocchio_data_reduced_;
 }
 
-bool RobotModelBuilder::getRobotHasFreeFlyer() {
-  return in_robot_has_free_flyer_;
+bool RobotModelBuilder::getRobotHasFreeFlyer() { return robot_has_free_flyer_; }
+
+std::map<int, int> RobotModelBuilder::getPinocchioToHarwdareInterfaceMap() {
+  return pin_to_hwi_;
 }
 
 }  // namespace linear_feedback_controller
