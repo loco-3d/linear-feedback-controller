3f45a59d5a7fbfec0179e342ff2ed5e250706256
Maximilien Naveau <maximilien.naveau@gmail.com>
2024-11-27 17:19:55 +0100

    Fix licensing

--- include/linear_feedback_controller/linear_feedback_controller_ros.hpp
+++ include/linear_feedback_controller/linear_feedback_controller_ros.hpp
@@ -1,10 +1,3 @@
-// Copyright (c) 2024 PAL Robotics S.L. All rights reserved.
-//
-// Unauthorized copying of this file, via any medium is strictly prohibited,
-// unless it was supplied under the terms of a license agreement or
-// nondisclosure agreement with PAL Robotics SL. In this case it may not be
-// copied or disclosed except in accordance with the terms of that agreement.
-
 #ifndef LINEAR_FEEDBACK_CONTROLLER_LINEAR_FEEDBACK_CONTROLLER_ROS_HPP
 #define LINEAR_FEEDBACK_CONTROLLER_LINEAR_FEEDBACK_CONTROLLER_ROS_HPP
 
--- src/linear_feedback_controller_ros.cpp
+++ src/linear_feedback_controller_ros.cpp
@@ -1,10 +1,3 @@
-// Copyright (c) 2024 PAL Robotics S.L. All rights reserved.
-//
-// Unauthorized copying of this file, via any medium is strictly prohibited,
-// unless it was supplied under the terms of a license agreement or
-// nondisclosure agreement with PAL Robotics SL. In this case it may not be
-// copied or disclosed except in accordance with the terms of that agreement.
-
 #include "linear_feedback_controller/linear_feedback_controller_ros.hpp"
 
 #include "ament_index_cpp/get_package_share_directory.hpp"
