3f86ad81cdc8abbdf9119c2ed050bffbf25e5b1e
Arthur Valiente <valiente.arthur@gmail.com>
2024-12-06 10:32:06 +0100

    [pd_controller] Add Ctor unittests

--- tests/test_pd_controller.cpp
+++ tests/test_pd_controller.cpp
@@ -1,5 +1,22 @@
 #include "gtest/gtest.h"
 
+#include <memory>
+
 #include "linear_feedback_controller/pd_controller.hpp"
 
-TEST(TODO, TODO) {}
+using namespace linear_feedback_controller;
+
+class UninitializedPdControllerTest : public ::testing::Test
+{
+protected:
+  void SetUp() override {}
+  void TearDown() override {}
+
+  std::unique_ptr<PDController> pd_ctrl_ptr_ = nullptr;
+};
+
+using PdControllerTest = UninitializedPdControllerTest;
+TEST_F(PdControllerTest, Ctor)
+{
+  EXPECT_NO_THROW({ pd_ctrl_ptr_ = std::make_unique<PDController>(); });
+}
