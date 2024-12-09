bf1bc084823563f6c0261c158e00e60f98061e63
Arthur Valiente <valiente.arthur@gmail.com>
2024-12-09 16:54:07 +0100

    [pd_controller] Apply clang-format @ Google style

--- tests/test_pd_controller.cpp
+++ tests/test_pd_controller.cpp
@@ -64,44 +64,47 @@ TEST(PdControllerTest, SetGains) {
 
   EXPECT_NO_THROW({ pd_ctrl.set_gains(Eigen::VectorXd{}, Eigen::VectorXd{}); });
 
-  for (const auto size : MakeArray<Gains::IdxType>(1, 3, 4, 50, 1000))
-  {
+  for (const auto size : MakeArray<Gains::IdxType>(1, 3, 4, 50, 1000)) {
     SCOPED_TRACE(::testing::Message() << "Same gains size: " << size);
     auto requested_gains = Gains::Random(size);
-    EXPECT_NO_THROW({ pd_ctrl.set_gains(requested_gains.p, requested_gains.d); });
+    EXPECT_NO_THROW(
+        { pd_ctrl.set_gains(requested_gains.p, requested_gains.d); });
     // FIXME: How to test the validity ??
 
     // Test for special double values acceptance
     for (const auto special_value : {
-           std::numeric_limits<double>::infinity(),
-           std::numeric_limits<double>::quiet_NaN(),
-           std::numeric_limits<double>::signaling_NaN(),
-         })
-    {
+             std::numeric_limits<double>::infinity(),
+             std::numeric_limits<double>::quiet_NaN(),
+             std::numeric_limits<double>::signaling_NaN(),
+         }) {
       SCOPED_TRACE(::testing::Message() << "Special value: " << special_value);
 
       {
         SCOPED_TRACE("P");
         auto _ = TemporaryMutate{requested_gains.p(0), special_value};
-        EXPECT_NO_THROW({ pd_ctrl.set_gains(requested_gains.p, requested_gains.d); });
+        EXPECT_NO_THROW(
+            { pd_ctrl.set_gains(requested_gains.p, requested_gains.d); });
       }
 
       {
         SCOPED_TRACE("D");
         auto _ = TemporaryMutate{requested_gains.d(0), special_value};
-        EXPECT_NO_THROW({ pd_ctrl.set_gains(requested_gains.p, requested_gains.d); });
+        EXPECT_NO_THROW(
+            { pd_ctrl.set_gains(requested_gains.p, requested_gains.d); });
       }
     }
   }
 
   using SizePair = std::array<Gains::IdxType, 2>;
-  for (const auto [p_size, d_size] :
-       MakeArray<SizePair>(SizePair{1, 2}, SizePair{4, 3}, SizePair{50, 1000}))
-  {
-    SCOPED_TRACE(::testing::Message() << "Different Sizes: P = " << p_size << ", D = " << d_size);
+  for (const auto [p_size, d_size] : MakeArray<SizePair>(
+           SizePair{1, 2}, SizePair{4, 3}, SizePair{50, 1000})) {
+    SCOPED_TRACE(::testing::Message()
+                 << "Different Sizes: P = " << p_size << ", D = " << d_size);
     const auto requested_gains = Gains::Random(p_size, d_size);
-    // FIXME: I guess it should fail but set_gains doesn't provide any feedback on failure
-    EXPECT_NO_THROW({ pd_ctrl.set_gains(requested_gains.p, requested_gains.d); });
+    // FIXME: I guess it should fail but set_gains doesn't provide any feedback
+    // on failure
+    EXPECT_NO_THROW(
+        { pd_ctrl.set_gains(requested_gains.p, requested_gains.d); });
   }
 }
 
@@ -111,9 +114,8 @@ struct References {
   Eigen::VectorXd tau;
   Eigen::VectorXd q;
 
-  static auto Random(IdxType tau_size,
-                     std::optional<IdxType> q_size = std::nullopt)
-      -> References {
+  static auto Random(IdxType tau_size, std::optional<IdxType> q_size =
+                                           std::nullopt) -> References {
     return References{
         .tau = Eigen::VectorXd::Random(tau_size),
         .q = Eigen::VectorXd::Random(q_size.value_or(tau_size)),
@@ -124,56 +126,58 @@ struct References {
 TEST(PdControllerTest, SetReferences) {
   auto pd_ctrl = PDController();
 
-  EXPECT_NO_THROW({ pd_ctrl.set_reference(Eigen::VectorXd{}, Eigen::VectorXd{}); });
+  EXPECT_NO_THROW(
+      { pd_ctrl.set_reference(Eigen::VectorXd{}, Eigen::VectorXd{}); });
 
-  for (const auto size : MakeArray<References::IdxType>(1, 3, 4, 50, 1000))
-  {
+  for (const auto size : MakeArray<References::IdxType>(1, 3, 4, 50, 1000)) {
     SCOPED_TRACE(::testing::Message() << "Same gains size: " << size);
 
     auto requested_refs = References::Random(size);
-    EXPECT_NO_THROW({ pd_ctrl.set_reference(requested_refs.tau, requested_refs.q); });
+    EXPECT_NO_THROW(
+        { pd_ctrl.set_reference(requested_refs.tau, requested_refs.q); });
     // FIXME: How to test the validity ??
 
     // Test for special double values acceptance
     for (const auto special_value : {
-           std::numeric_limits<double>::infinity(),
-           std::numeric_limits<double>::quiet_NaN(),
-           std::numeric_limits<double>::signaling_NaN(),
-         })
-    {
+             std::numeric_limits<double>::infinity(),
+             std::numeric_limits<double>::quiet_NaN(),
+             std::numeric_limits<double>::signaling_NaN(),
+         }) {
       SCOPED_TRACE(::testing::Message() << "Special value: " << special_value);
 
       {
         SCOPED_TRACE("TAU");
         auto _ = TemporaryMutate{requested_refs.tau(0), special_value};
-        EXPECT_NO_THROW({ pd_ctrl.set_reference(requested_refs.tau, requested_refs.q); });
+        EXPECT_NO_THROW(
+            { pd_ctrl.set_reference(requested_refs.tau, requested_refs.q); });
       }
 
       {
         SCOPED_TRACE("Q");
         auto _ = TemporaryMutate{requested_refs.q(0), special_value};
-        EXPECT_NO_THROW({ pd_ctrl.set_reference(requested_refs.tau, requested_refs.q); });
+        EXPECT_NO_THROW(
+            { pd_ctrl.set_reference(requested_refs.tau, requested_refs.q); });
       }
     }
   }
 
   using SizePair = std::array<References::IdxType, 2>;
-  for (const auto [tau_size, q_size] :
-       MakeArray<SizePair>(SizePair{1, 2}, SizePair{4, 3}, SizePair{50, 1000}))
-  {
-    SCOPED_TRACE(
-      ::testing::Message() << "Different Sizes: TAU = " << tau_size << ", Q = " << q_size);
+  for (const auto [tau_size, q_size] : MakeArray<SizePair>(
+           SizePair{1, 2}, SizePair{4, 3}, SizePair{50, 1000})) {
+    SCOPED_TRACE(::testing::Message() << "Different Sizes: TAU = " << tau_size
+                                      << ", Q = " << q_size);
     const auto requested_refs = References::Random(tau_size, q_size);
-    // FIXME: I guess it should fail but set_ref doesn't provide any feedback on failure
-    EXPECT_NO_THROW({ pd_ctrl.set_reference(requested_refs.tau, requested_refs.q); });
+    // FIXME: I guess it should fail but set_ref doesn't provide any feedback on
+    // failure
+    EXPECT_NO_THROW(
+        { pd_ctrl.set_reference(requested_refs.tau, requested_refs.q); });
   }
 }
 
 TEST(PdController, ComputeControl) {
   auto pd_ctrl = PDController();
 
-  for (const auto size : {1u, 2u, 5u, 20u})
-  {
+  for (const auto size : {1u, 2u, 5u, 20u}) {
     SCOPED_TRACE(::testing::Message() << "Problem size = " << size);
 
     const auto gains = Gains::Random(size);
