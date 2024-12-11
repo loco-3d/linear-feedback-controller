67afd21425b913c5c95f0324dc585c367b5a9a9c
Arthur Valiente <valiente.arthur@gmail.com>
2024-12-11 13:14:14 +0100

    [lf_controller] Add dummy initial unittest for the LF Controller

---
+++ tests/test_lf_controller.cpp
@@ -0,0 +1,9 @@
+#include "gtest/gtest.h"
+
+#include "linear_feedback_controller/lf_controller.hpp"
+
+#include "example-robot-data/path.hpp"
+
+using namespace linear_feedback_controller;
+
+TEST(LfControllerTest, TODO) {}
