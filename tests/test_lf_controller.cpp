2dd175c72e2228656b195e63c5813b0af102053a
Arthur Valiente <valiente.arthur@gmail.com>
2024-12-16 10:05:19 +0100

    [lf_controller] Add Init with empty model test

--- tests/test_lf_controller.cpp
+++ tests/test_lf_controller.cpp
@@ -1,9 +1,24 @@
-#include "example-robot-data/path.hpp"
+#include <memory>
+#include <string_view>
+
 #include "gtest/gtest.h"
 #include "linear_feedback_controller/lf_controller.hpp"
 
 using namespace linear_feedback_controller;
 
+// EXAMPLE_ROBOT_DATA_MODEL_DIR is a compile definition imported when linking to
+// example-robot-data
+constexpr auto ROBOT_MODEL_DIR_PATH =
+    std::string_view{EXAMPLE_ROBOT_DATA_MODEL_DIR};
+
 TEST(LfControllerTest, Ctor) {
   EXPECT_NO_THROW({ auto ctrl = LFController(); });
+}
+
+TEST(LfControllerTest, InitializeEmptyModel) {
+  auto ctrl = LFController();
+  EXPECT_ANY_THROW({
+    // Empty Robot model
+    ctrl.initialize(std::make_shared<RobotModelBuilder>());
+  });
 }
