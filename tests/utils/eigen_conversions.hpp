4d3478b3bc9ea5a8c602658a0b1f017573ba4ed2
Arthur Valiente <valiente.arthur@gmail.com>
2025-01-14 09:01:53 +0100

    [lf_controller] Add a UnaryOp getter to Make* functions

--- tests/utils/eigen_conversions.hpp
+++ tests/utils/eigen_conversions.hpp
@@ -1,10 +1,12 @@
 #pragma once
 
-#include <iterator>  // std::distance
+#include <iterator>  // std::distance, std::iterator_traits
 #include <ostream>
 #include <string_view>
+#include <utility>  // std::forward
 
-#include "eigen.hpp"
+#include "Eigen/Geometry"  // Eigen::Quaternions
+#include "eigen.hpp"       // PrintTo(Eigen::Vector)
 #include "linear_feedback_controller_msgs/eigen_conversions.hpp"
 
 // Using this namesapce for ADL
@@ -107,14 +109,21 @@ namespace tests::utils {
 /**
  *  @brief Create a randomized JointState struct for each names provided
  *
- *  @tparam InputIt Input iterator of T used to construct a std::string
- *  @param[in] [first, last) Iterator range of joints name
+ *  @tparam InputIt Input iterator containing the joints name
+ *  @tparam UnaryOp Unary operation transforming InputIt into a
+ *                  std::string_view compatible value
+ *
+ *  @param[in] [first, last) Range of names used as follow:
+ *                           std::string{std::string_view{get_name(*first)}}
+ *  @param[in] get_name Functor use to get a name from the dereferenced iterator
+ *
  *  @return linear_feedback_controller_msgs::Eigen::JointState Randomized
  */
-template <typename InputIt>
-auto MakeRandomJointStateWithNames(InputIt first, InputIt last)
+template <typename InputIt, typename UnaryOp>
+auto MakeRandomJointStateWithNames(InputIt first, InputIt last,
+                                   UnaryOp&& get_name)
     -> linear_feedback_controller_msgs::Eigen::JointState {
-  // TODO: static_assert() on InputIt to provide meaningfull error msg
+  // TODO: static_assert()
 
   linear_feedback_controller_msgs::Eigen::JointState joint_state;
   if (const auto number_of_joints = std::distance(first, last);
@@ -131,11 +140,35 @@ auto MakeRandomJointStateWithNames(InputIt first, InputIt last)
 
     joint_state.name.reserve(number_of_joints);
     for (; first != last; ++first) {
-      joint_state.name.emplace_back(*first);
+      joint_state.name.emplace_back(std::string_view{get_name(*first)});
     }
   }
 
   return joint_state;
+}
+
+/**
+ *  @brief Create a randomized JointState struct for each names provided
+ *
+ *  Basically call MakeRandomJointStateWithNames(first, last, identity).
+ *
+ *  @tparam InputIt Input iterator containing values convertible to
+ *                  std::string_view
+ *
+ *  @param[in] [first, last) Iterator range used to construct as follow:
+ *                           std::string{std::string_view{*first}}
+ *
+ *  @return linear_feedback_controller_msgs::Eigen::JointState Randomized
+ */
+template <typename InputIt>
+auto MakeRandomJointStateWithNames(InputIt first, InputIt last)
+    -> linear_feedback_controller_msgs::Eigen::JointState {
+  // Default identity UnaryOp -> forward the value / does nothing
+  // Same as std::identity (c++20)
+  return MakeRandomJointStateWithNames(
+      first, last, [](auto&& value) noexcept -> decltype(auto) {
+        return std::forward<decltype(value)>(value);
+      });
 }
 
 /// Used as argument of the PushNewJointStateTo() function below
@@ -168,35 +201,81 @@ inline auto PushNewJointStateTo(
 }
 
 /**
+ *  @brief Create a randomized Sensor struct for each names provided
+ *
+ *  @tparam InputIt Input iterator containing the joints name
+ *  @tparam UnaryOp Unary operation transforming InputIt into a
+ *                  std::string_view compatible value
+ *
+ *  @param[in] [first, last) Range of names used as follow:
+ *                           std::string{std::string_view{get_name(*first)}}
+ *  @param[in] get_name Functor use to get a name from the dereferenced iterator
+ *
+ *  @return linear_feedback_controller_msgs::Eigen::Sensor Randomized
+ */
+template <typename InputIt, typename UnaryOp>
+auto MakeRandomSensorForJoints(InputIt first, InputIt last, UnaryOp&& get_name)
+    -> linear_feedback_controller_msgs::Eigen::Sensor {
+  linear_feedback_controller_msgs::Eigen::Sensor sensor;
+
+  // base_pose is composed of a 3D (x,y,z) vector followed by a normalized
+  // quaternion
+  sensor.base_pose.head<3>() = Eigen::Vector3d::Random();
+  sensor.base_pose.tail<4>() = Eigen::Quaterniond::UnitRandom().coeffs();
+
+  sensor.base_twist = decltype(sensor.base_twist)::Random();
+
+  sensor.joint_state = MakeRandomJointStateWithNames(
+      first, last, std::forward<UnaryOp>(get_name));
+
+  // TODO: Contacts ???
+  return sensor;
+}
+
+/**
  *  @brief Create a randomized Sensor struct for each joints name
  *
- *  @tparam InputIt Input iterator of std::string compatible elements
+ *  Basically call MakeRandomSensorForJoints(first, last, identity).
  *
- *  @param[in] first, last Range of joint name
+ *  @tparam InputIt Input iterator containing values convertible to
+ *                  std::string_view
+ *
+ *  @param[in] [first, last) Range of names used as follow:
+ *                           std::string{std::string_view{*first}}
+ *
  *  @return linear_feedback_controller_msgs::Eigen::Sensor Randomized
  */
 template <typename InputIt>
 auto MakeRandomSensorForJoints(InputIt first, InputIt last)
     -> linear_feedback_controller_msgs::Eigen::Sensor {
-  linear_feedback_controller_msgs::Eigen::Sensor sensor;
-  sensor.base_pose = decltype(sensor.base_pose)::Random();
-  sensor.base_twist = decltype(sensor.base_twist)::Random();
-  sensor.joint_state = MakeRandomJointStateWithNames(first, last);
-  return sensor;
+  // Default identity UnaryOp -> forward the value / does nothing
+  // Same as std::identity (c++20)
+  return MakeRandomSensorForJoints(
+      first, last, [](auto&& value) noexcept -> decltype(auto) {
+        return std::forward<decltype(value)>(value);
+      });
 }
 
 /**
- *  @brief Create a randomized Control struct for the given joints
+ *  @brief Create a randomized Sensor struct for each names provided
  *
- *  @param[in] first, last Range of joint names
+ *  @tparam InputIt Input iterator containing the joints name
+ *  @tparam UnaryOp Unary operation transforming InputIt into a
+ *                  std::string_view compatible value
+ *
+ *  @param[in] [first, last) Range of names used as follow:
+ *                           std::string{std::string_view{get_name(*first)}}
+ *  @param[in] get_name Functor use to get a name from the dereferenced iterator
+ *
  *  @return linear_feedback_controller_msgs::Eigen::Control Randomized
  */
-template <typename InputIt>
-auto MakeRandomControlForJoints(InputIt first, InputIt last)
+template <typename InputIt, typename UnaryOp>
+auto MakeRandomControlForJoints(InputIt first, InputIt last, UnaryOp&& get_name)
     -> linear_feedback_controller_msgs::Eigen::Control {
   linear_feedback_controller_msgs::Eigen::Control control;
 
-  control.initial_state = MakeRandomSensorForJoints(first, last);
+  control.initial_state =
+      MakeRandomSensorForJoints(first, last, std::forward<UnaryOp>(get_name));
 
   const auto number_of_joints = control.initial_state.joint_state.name.size();
   control.feedforward = Eigen::VectorXd::Random(number_of_joints);
@@ -205,4 +284,29 @@ auto MakeRandomControlForJoints(InputIt first, InputIt last)
 
   return control;
 }
+
+/**
+ *  @brief Create a randomized JointState struct for each names provided
+ *
+ *  Basically call MakeRandomControlForJoints(first, last, identity).
+ *
+ *  @tparam InputIt Input iterator containing values convertible to
+ *                  std::string_view
+ *
+ *  @param[in] [first, last) Iterator range used to construct as follow:
+ *                           std::string{std::string_view{*first}}
+ *
+ *  @return linear_feedback_controller_msgs::Eigen::Control Randomized
+ */
+template <typename InputIt>
+auto MakeRandomControlForJoints(InputIt first, InputIt last)
+    -> linear_feedback_controller_msgs::Eigen::Control {
+  // Default identity UnaryOp -> forward the value / does nothing
+  // Same as std::identity (c++20)
+  return MakeRandomControlForJoints(
+      first, last, [](auto&& value) noexcept -> decltype(auto) {
+        return std::forward<decltype(value)>(value);
+      });
+}
+
 }  // namespace tests::utils
