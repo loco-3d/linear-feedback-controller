1f33867eeac2c749f9d6b23fe412fde5d3ae5eda
Arthur Valiente <valiente.arthur@gmail.com>
2025-01-17 15:09:15 +0100

    [lf_controller] Refactor to use ModelDescription as Test params

--- tests/utils/robot_model.hpp
+++ tests/utils/robot_model.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
-#include <iomanip>   // std::quoted
-#include <iterator>  // std::distance
+#include <array>
+#include <iomanip>  // std::quoted
 #include <memory>
 #include <optional>
 #include <ostream>
@@ -9,6 +9,7 @@
 #include <type_traits>
 
 #include "linear_feedback_controller/robot_model_builder.hpp"
+#include "view.hpp"
 
 namespace tests::utils {
 
@@ -91,39 +92,75 @@ constexpr auto PrintTo(JointDescription joint, std::ostream *os) noexcept
 
   *os << "JointDescription{";
   *os << ".name = " << std::quoted(joint.name) << ", ";
-  *os << ".type = " << std::quoted(ToString(joint.type)) << ", ";
+
+  *os << ".type = ";
+  PrintTo(joint.type, os);
+  *os << ", ";
+
   *os << "}";
 }
 
 /// Global information about the model we wish to create
-/// NOTE: Easy to copy
+/// IMPORTANT: Should not be used directly but as function argument
+template <typename Range>
 struct ModelDescription {
+  static_assert(
+      std::is_base_of_v<typename View<Range>::ValueType, JointDescription>,
+      "The Range provided to joint_list MUST contains JointDescription in it");
+
+  /// PrintFormat used by PrintTo to format a Model
+  struct PrintFormat;
+
   std::string_view urdf; /*!< Complete Robot URDF description */
+  Range joint_list;      /*!< Joint description list */
   bool has_free_flyer;   /*!< Indicates if the model uses free flyer */
 };
 
-/// PrintFormat used by PrintTo to format a Model
-struct ModelDescriptionPrintFormat {
+/// CTAD use to create a ModelDescription using the Range type
+template <typename Range>
+ModelDescription(std::string_view, Range, bool) -> ModelDescription<Range>;
+
+/**
+ *  @brief Create an array of ModelDescription<> with and without free flyer,
+ *         for each joint_list provided
+ *
+ *  @tparam ListType The joint_list type used by ModelDescription<ListType>
+ *  @tparam Lists... All list arguments types
+ *
+ *  @param[in] urdf The common URDF used by every ModelDescription
+ *  @param[in] joint_lists... All arguments forwarded to the '.joint_list = '
+ *                            constructor
+ *
+ *  @return std::array<ModelDescription<ListType>, 2 * sizeof...(Lists)>
+ *          Containing all ModelDescriptions constructed
+ */
+template <typename ListType, typename... Lists>
+constexpr auto MakeAllModelDescriptionsFor(std::string_view urdf,
+                                           Lists &&...joint_lists) noexcept
+    -> std::array<ModelDescription<ListType>, 2 * (sizeof...(Lists))> {
+  static_assert((... and std::is_constructible_v<ListType, Lists>));
+
+  return {
+      // NOTE: do not forward the first joint_list, otherwise rvalue may be
+      // moved away
+      ModelDescription<ListType>{
+          .urdf = urdf,
+          .joint_list = joint_lists,
+          .has_free_flyer = false,
+      }...,
+      ModelDescription<ListType>{
+          .urdf = urdf,
+          .joint_list = std::forward<Lists>(joint_lists),
+          .has_free_flyer = true,
+      }...,
+  };
+}
+
+/// Declaration of the PrintFormat
+template <typename Range>
+struct ModelDescription<Range>::PrintFormat {
   bool full_urdf = false; /*!< Print the full URDF string */
 };
-
-constexpr auto GetRobotNameFromURDF(std::string_view urdf)
-    -> std::optional<std::string_view> {
-  std::optional<std::string_view> res = std::nullopt;
-
-  constexpr std::string_view urdf_robot_name_tag = "<robot name=\"";
-  if (const auto begin_pos = urdf.find(urdf_robot_name_tag);
-      begin_pos != std::string_view::npos) {
-    urdf = urdf.substr(begin_pos + urdf_robot_name_tag.size());
-
-    if (const auto end_pos = urdf.find_first_of("\">");
-        (end_pos != std::string_view::npos) and (urdf[end_pos] != '>')) {
-      res = urdf.substr(0, end_pos);
-    }
-  }
-
-  return res;
-}
 
 /**
  *  @brief Print a given \a model into \a os
@@ -132,24 +169,49 @@ constexpr auto GetRobotNameFromURDF(std::string_view urdf)
  *  @param[inout] os The output stream ptr we wish to print to
  *  @param[in] fmt The format specifier use to print the model
  */
-constexpr auto PrintTo(ModelDescription model, std::ostream *os,
-                       ModelDescriptionPrintFormat fmt = {}) noexcept -> void {
+template <typename T>
+constexpr auto PrintTo(
+    const ModelDescription<T> &model, std::ostream *os,
+    typename ModelDescription<T>::PrintFormat fmt = {}) noexcept -> void {
   if (os == nullptr) return;
 
   *os << "ModelDescription{";
+
+  // Dumb function to retreive the robot name from the URDF
+  constexpr auto GetRobotNameFromURDF =
+      [](std::string_view urdf) -> std::optional<std::string_view> {
+    std::optional<std::string_view> res = std::nullopt;
+
+    constexpr std::string_view urdf_robot_name_tag = "<robot name=\"";
+    if (const auto begin_pos = urdf.find(urdf_robot_name_tag);
+        begin_pos != std::string_view::npos) {
+      urdf = urdf.substr(begin_pos + urdf_robot_name_tag.size());
+
+      if (const auto end_pos = urdf.find_first_of("\">");
+          (end_pos != std::string_view::npos) and (urdf[end_pos] != '>')) {
+        res = urdf.substr(0, end_pos);
+      }
+    }
+
+    return res;
+  };
 
   *os << ".urdf = ";
   if (fmt.full_urdf) {
     *os << std::quoted(model.urdf);
   } else if (const auto robot_name = GetRobotNameFromURDF(model.urdf);
              robot_name.has_value()) {
-    *os << "{Robot = \"" << *robot_name << "\"}";
+    *os << *robot_name;
   } else {
     *os << "str{";
     *os << ".data() = @ " << (void const *)model.urdf.data() << ", ";
     *os << ".size() = " << model.urdf.size() << ", ";
     *os << "}";
   }
+  *os << ", ";
+
+  *os << ".joint_list = ";
+  PrintTo(View{model.joint_list}, os);
   *os << ", ";
 
   *os << ".has_free_flyer = " << model.has_free_flyer;
@@ -162,24 +224,16 @@ constexpr auto PrintTo(ModelDescription model, std::ostream *os,
  *         and a list of Joints to control
  *
  *  @param[in] model The model description (URDF) we wish to build
- *  @param[in] [first, last) A valid range of JointDescription
  *
- *  @return std::optional<RobotModelBuilder> A valid RobotModelBuilder (i.e.
- *          build_model() returned true), nullopt otherwise
+ *  @return std::unique_ptr<RobotModelBuilder> A valid RobotModelBuilder (i.e.
+ *          build_model() returned true), nullptr otherwise
  */
-template <typename InputIt>
-auto MakeRobotModelBuilderFrom(ModelDescription model, InputIt first,
-                               InputIt last)
+template <typename T>
+auto MakeRobotModelBuilderFrom(ModelDescription<T> model)
     -> std::unique_ptr<linear_feedback_controller::RobotModelBuilder> {
-  using Category = typename std::iterator_traits<InputIt>::iterator_category;
-  static_assert(std::is_base_of_v<std::input_iterator_tag, Category>);
-
-  using ValueType = typename std::iterator_traits<InputIt>::value_type;
-  static_assert(std::is_base_of_v<JointDescription, ValueType>);
-
-  const auto number_of_joints = std::distance(first, last);
+  const auto number_of_joints = ssize(View{model.joint_list});
   assert(number_of_joints >= 0 &&
-         "Wrong input range [first, last) (negative size)");
+         "Wrong joint_list input range (negative size)");
 
   auto controlled_joints = std::vector<std::string>{};
   controlled_joints.reserve(number_of_joints);
@@ -187,9 +241,7 @@ auto MakeRobotModelBuilderFrom(ModelDescription model, InputIt first,
   auto moving_joints = std::vector<std::string>{};
   moving_joints.reserve(number_of_joints);
 
-  for (; first != last; ++first) {
-    const JointDescription &joint = *first;
-
+  for (const auto &joint : model.joint_list) {
     if (IsControlled(joint.type)) {
       controlled_joints.emplace_back(joint.name);
     }
@@ -208,12 +260,4 @@ auto MakeRobotModelBuilderFrom(ModelDescription model, InputIt first,
     return nullptr;
   }
 }
-
-template <typename Range>
-auto MakeRobotModelBuilderFrom(ModelDescription model, Range &&range) {
-  return MakeRobotModelBuilderFrom(model,
-                                   std::cbegin(std::forward<Range>(range)),
-                                   std::cend(std::forward<Range>(range)));
-}
-
 }  // namespace tests::utils
