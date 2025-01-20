5180e3befc992eda01aa7dfcfa5d2fa663bfb8c3
Arthur Valiente <valiente.arthur@gmail.com>
2025-01-20 15:21:04 +0100

    [lfc] Remove example-robot-data deps

--- tests/test_linear_feedback_controller.cpp
+++ tests/test_linear_feedback_controller.cpp
@@ -1,7 +1,6 @@
 #include "linear_feedback_controller/linear_feedback_controller.hpp"
 using linear_feedback_controller::LinearFeedbackController;
 
-#include "example-robot-data/path.hpp"
 #include "gtest/gtest.h"
 
 TEST(LinearFeedbackControllerTest, Ctor) {
