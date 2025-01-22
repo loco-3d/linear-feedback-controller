018d08ad0f83dcd3b1bd86c3613f3553ff2c2a19
Arthur Valiente <valiente.arthur@gmail.com>
2025-01-22 09:41:34 +0100

    [lfc] Add .Load* tests templates

--- tests/test_linear_feedback_controller.cpp
+++ tests/test_linear_feedback_controller.cpp
@@ -1,4 +1,5 @@
 #include "linear_feedback_controller/linear_feedback_controller.hpp"
+using linear_feedback_controller::ControllerParameters;
 using linear_feedback_controller::LinearFeedbackController;
 
 #include "gtest/gtest.h"
@@ -10,4 +11,32 @@ TEST(LinearFeedbackControllerTest, Ctor) {
 TEST(LinearFeedbackControllerTest, LoadEmptyParams) {
   auto ctrl = LinearFeedbackController{};
   EXPECT_FALSE(ctrl.load({}));
+}
+
+TEST(LinearFeedbackControllerTest, DISABLED_LoadNoURDF) {
+  auto ctrl = LinearFeedbackController{};
+  auto params = ControllerParameters{};
+  // TODO
+  EXPECT_FALSE(ctrl.load(params));
+}
+
+TEST(LinearFeedbackControllerTest, DISABLED_LoadSizeMismatch) {
+  auto ctrl = LinearFeedbackController{};
+  auto params = ControllerParameters{};
+  // TODO
+  EXPECT_FALSE(ctrl.load(params));
+}
+
+TEST(LinearFeedbackControllerTest, DISABLED_LoadNegativeDuration) {
+  auto ctrl = LinearFeedbackController{};
+  auto params = ControllerParameters{};
+  // TODO
+  EXPECT_FALSE(ctrl.load(params));
+}
+
+TEST(LinearFeedbackControllerTest, Load) {
+  auto ctrl = LinearFeedbackController{};
+  auto params = ControllerParameters{};
+  // TODO
+  EXPECT_TRUE(ctrl.load(params));
 }
