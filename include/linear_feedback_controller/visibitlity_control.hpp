f7d9d6b9be3dde4615b7128fb5d9591aaeaa6bf5
Arthur Valiente <valiente.arthur@gmail.com>
2025-02-24 08:56:08 +0100

    Ease reading the define if/else blocks in visibility_control

--- include/linear_feedback_controller/visibitlity_control.hpp
+++ include/linear_feedback_controller/visibitlity_control.hpp
@@ -5,6 +5,7 @@
 //     https://gcc.gnu.org/wiki/Visibility
 
 #if defined _WIN32 || defined __CYGWIN__
+
 #ifdef __GNUC__
 #define LINEAR_FEEDBACK_CONTROLLER_EXPORT __attribute__((dllexport))
 #define LINEAR_FEEDBACK_CONTROLLER_IMPORT __attribute__((dllimport))
@@ -12,16 +13,21 @@
 #define LINEAR_FEEDBACK_CONTROLLER_EXPORT __declspec(dllexport)
 #define LINEAR_FEEDBACK_CONTROLLER_IMPORT __declspec(dllimport)
 #endif
+
 #ifdef LINEAR_FEEDBACK_CONTROLLER_BUILDING_DLL
 #define LINEAR_FEEDBACK_CONTROLLER_PUBLIC LINEAR_FEEDBACK_CONTROLLER_EXPORT
 #else
 #define LINEAR_FEEDBACK_CONTROLLER_PUBLIC LINEAR_FEEDBACK_CONTROLLER_IMPORT
 #endif
+
 #define LINEAR_FEEDBACK_CONTROLLER_PUBLIC_TYPE LINEAR_FEEDBACK_CONTROLLER_PUBLIC
 #define LINEAR_FEEDBACK_CONTROLLER_LOCAL
-#else
+
+#else  // defined _WIN32 || defined __CYGWIN__
+
 #define LINEAR_FEEDBACK_CONTROLLER_EXPORT __attribute__((visibility("default")))
 #define LINEAR_FEEDBACK_CONTROLLER_IMPORT
+
 #if __GNUC__ >= 4
 #define LINEAR_FEEDBACK_CONTROLLER_PUBLIC __attribute__((visibility("default")))
 #define LINEAR_FEEDBACK_CONTROLLER_LOCAL __attribute__((visibility("hidden")))
@@ -29,7 +35,9 @@
 #define LINEAR_FEEDBACK_CONTROLLER_PUBLIC
 #define LINEAR_FEEDBACK_CONTROLLER_LOCAL
 #endif
+
 #define LINEAR_FEEDBACK_CONTROLLER_PUBLIC_TYPE
-#endif
+
+#endif  // defined _WIN32 || defined __CYGWIN__
 
 #endif  // LINEAR_FEEDBACK_CONTROLLER__VISIBILITY_CONTROL_HPP_
