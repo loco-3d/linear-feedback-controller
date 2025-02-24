5ef13921eacdc60f5a15902fcf983ce8869c3396
Arthur Valiente <valiente.arthur@gmail.com>
2025-02-24 09:52:28 +0100

    Update visibility_control according to GCC wiki

--- include/linear_feedback_controller/visibitlity_control.hpp
+++ include/linear_feedback_controller/visibitlity_control.hpp
@@ -4,40 +4,55 @@
 // This logic was borrowed (then namespaced) from the examples on the gcc wiki:
 //     https://gcc.gnu.org/wiki/Visibility
 
+// Define LINEAR_FEEDBACK_CONTROLLER_[EXPORT, IMPORT, LOCAL]
 #if defined _WIN32 || defined __CYGWIN__
 
 #ifdef __GNUC__
-#define LINEAR_FEEDBACK_CONTROLLER_EXPORT __attribute__((dllexport))
-#define LINEAR_FEEDBACK_CONTROLLER_IMPORT __attribute__((dllimport))
+#define LINEAR_FEEDBACK_CONTROLLER_HELPER_EXPORT __attribute__((dllexport))
+#define LINEAR_FEEDBACK_CONTROLLER_HELPER_IMPORT __attribute__((dllimport))
 #else
-#define LINEAR_FEEDBACK_CONTROLLER_EXPORT __declspec(dllexport)
-#define LINEAR_FEEDBACK_CONTROLLER_IMPORT __declspec(dllimport)
+#define LINEAR_FEEDBACK_CONTROLLER_HELPER_EXPORT __declspec(dllexport)
+#define LINEAR_FEEDBACK_CONTROLLER_HELPER_IMPORT __declspec(dllimport)
 #endif
 
-#ifdef LINEAR_FEEDBACK_CONTROLLER_BUILDING_DLL
-#define LINEAR_FEEDBACK_CONTROLLER_PUBLIC LINEAR_FEEDBACK_CONTROLLER_EXPORT
-#else
-#define LINEAR_FEEDBACK_CONTROLLER_PUBLIC LINEAR_FEEDBACK_CONTROLLER_IMPORT
-#endif
-
-#define LINEAR_FEEDBACK_CONTROLLER_PUBLIC_TYPE LINEAR_FEEDBACK_CONTROLLER_PUBLIC
-#define LINEAR_FEEDBACK_CONTROLLER_LOCAL
+// All symbols are hidden by default in windows
+#define LINEAR_FEEDBACK_CONTROLLER_HELPER_LOCAL
 
 #else  // defined _WIN32 || defined __CYGWIN__
 
-#define LINEAR_FEEDBACK_CONTROLLER_EXPORT __attribute__((visibility("default")))
-#define LINEAR_FEEDBACK_CONTROLLER_IMPORT
-
 #if __GNUC__ >= 4
-#define LINEAR_FEEDBACK_CONTROLLER_PUBLIC __attribute__((visibility("default")))
-#define LINEAR_FEEDBACK_CONTROLLER_LOCAL __attribute__((visibility("hidden")))
+#define LINEAR_FEEDBACK_CONTROLLER_HELPER_EXPORT \
+  __attribute__((visibility("default")))
+#define LINEAR_FEEDBACK_CONTROLLER_HELPER_IMPORT \
+  __attribute__((visibility("default")))
+#define LINEAR_FEEDBACK_CONTROLLER_HELPER_LOCAL \
+  __attribute__((visibility("hidden")))
 #else
-#define LINEAR_FEEDBACK_CONTROLLER_PUBLIC
-#define LINEAR_FEEDBACK_CONTROLLER_LOCAL
+#define LINEAR_FEEDBACK_CONTROLLER_HELPER_EXPORT
+#define LINEAR_FEEDBACK_CONTROLLER_HELPER_IMPORT
+#define LINEAR_FEEDBACK_CONTROLLER_HELPER_LOCAL
 #endif
 
-#define LINEAR_FEEDBACK_CONTROLLER_PUBLIC_TYPE
-
 #endif  // defined _WIN32 || defined __CYGWIN__
+
+// Define LINEAR_FEEDBACK_CONTROLLER_[API, LOCAL] based on the
+#ifdef LINEAR_FEEDBACK_CONTROLLER_IS_SHARED
+// LFC lib is shared (.so)
+
+#ifdef LINEAR_FEEDBACK_CONTROLLER_EXPORT  // Set only when compiling the lib
+// We are building the shared lib -> EXPORT symbols
+#define LINEAR_FEEDBACK_CONTROLLER_API LINEAR_FEEDBACK_CONTROLLER_HELPER_EXPORT
+#else
+// We are linking to the shared lib -> IMPORT symbols
+#define LINEAR_FEEDBACK_CONTROLLER_API LINEAR_FEEDBACK_CONTROLLER_HELPER_IMPORT
+#endif
+
+#define LINEAR_FEEDBACK_CONTROLLER_LOCAL LINEAR_FEEDBACK_CONTROLLER_HELPER_LOCAL
+
+#else  // LINEAR_FEEDBACK_CONTROLLER_IS_SHARED
+// LFC lib is static (.a)
+#define LINEAR_FEEDBACK_CONTROLLER_LOCAL
+#define LINEAR_FEEDBACK_CONTROLLER_API
+#endif
 
 #endif  // LINEAR_FEEDBACK_CONTROLLER__VISIBILITY_CONTROL_HPP_
