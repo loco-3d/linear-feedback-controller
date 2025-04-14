14b9ba620fac01a1e4c901aae2d8b9b1e82b9247
Maximilien Naveau <maximilien.naveau@gmail.com>
2025-04-14 14:12:06 +0200

    Simplify the nix code

--- flake.nix
+++ flake.nix
@@ -31,28 +31,8 @@
             ];
           };
           checks = lib.mapAttrs' (n: lib.nameValuePair "package-${n}") self'.packages;
-          packages = {
-            default = self'.packages.humble-linear-feedback-controller;
-            humble-linear-feedback-controller =
-              pkgs.rosPackages.humble.linear-feedback-controller.overrideAttrs
-                {
-                  src = lib.fileset.toSource {
-                    root = ./.;
-                    fileset = lib.fileset.unions [
-                      ./cmake
-                      ./CMakeLists.txt
-                      ./config
-                      ./controller_plugins.xml
-                      ./include
-                      ./launch
-                      ./LICENSE
-                      ./package.xml
-                      ./src
-                      ./tests
-                    ];
-                  };
-                };
-            jazzy-linear-feedback-controller = pkgs.rosPackages.jazzy.linear-feedback-controller.overrideAttrs {
+          packages =
+            let
               src = lib.fileset.toSource {
                 root = ./.;
                 fileset = lib.fileset.unions [
@@ -68,8 +48,16 @@
                   ./tests
                 ];
               };
+            in
+            {
+              default = self'.packages.humble-linear-feedback-controller;
+              humble-linear-feedback-controller =
+                pkgs.rosPackages.humble.linear-feedback-controller.overrideAttrs
+                  { inherit src; };
+              jazzy-linear-feedback-controller = pkgs.rosPackages.jazzy.linear-feedback-controller.overrideAttrs {
+                inherit src;
+              };
             };
-          };
           treefmt.programs = {
             deadnix.enable = true;
             nixfmt.enable = true;
