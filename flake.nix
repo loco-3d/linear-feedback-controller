581348534869d5d00c04d4020e89c06ead3b2cdd
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-06-18 15:16:04 +0200

    nix: use gepetto module

--- flake.nix
+++ flake.nix
@@ -2,35 +2,27 @@
   description = "RosControl linear feedback controller with pal base estimator and RosTopics external interface.";
 
   inputs = {
-    gepetto.url = "github:gepetto/nix";
+    gepetto.url = "github:gepetto/nix/module";
     flake-parts.follows = "gepetto/flake-parts";
     nixpkgs.follows = "gepetto/nixpkgs";
     nix-ros-overlay.follows = "gepetto/nix-ros-overlay";
+    systems.follows = "gepetto/systems";
     treefmt-nix.follows = "gepetto/treefmt-nix";
   };
 
   outputs =
     inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" ];
-      imports = [ inputs.treefmt-nix.flakeModule ];
+      systems = import inputs.systems;
+      imports = [ inputs.gepetto.flakeModule ];
       perSystem =
         {
           lib,
           pkgs,
-          system,
           self',
           ...
         }:
         {
-          _module.args.pkgs = import inputs.nixpkgs {
-            inherit system;
-            overlays = [
-              inputs.nix-ros-overlay.overlays.default
-              inputs.gepetto.overlays.default
-            ];
-          };
-          checks = lib.mapAttrs' (n: lib.nameValuePair "package-${n}") self'.packages;
           packages =
             let
               src = lib.fileset.toSource {
@@ -58,10 +50,6 @@
                 inherit src;
               };
             };
-          treefmt.programs = {
-            deadnix.enable = true;
-            nixfmt.enable = true;
-          };
         };
     };
 }
