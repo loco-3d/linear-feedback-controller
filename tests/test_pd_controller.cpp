d639bb7d2f0b0ee46d234c385dbc735da9a10067
Clement Pene <cpene@nilgiri>
2025-07-15 13:06:32 +0200

    test(pd_controller): rewrite Unit Tests
    Aiming to be easier to read - some tests are not passing due to required modification on C file

--- tests/test_pd_controller.cpp
+++ tests/test_pd_controller.cpp
@@ -1,214 +1,199 @@
-#include <limits>  // numeric_limits
-
-#include "utils/eigen.hpp"
-#include "utils/mutation.hpp"
-using tests::utils::TemporaryMutate;
-
-#include "utils/pd_controller.hpp"
-using tests::utils::ExpectedPDControlFrom;
-using tests::utils::Gains;
-using tests::utils::References;
-
-#include "linear_feedback_controller/pd_controller.hpp"
-using linear_feedback_controller::PDController;
-
 #include "gtest/gtest.h"
+#include "linear_feedback_controller/pd_controller.hpp" // Assurez-vous que le chemin est correct
+#include "Eigen/Core"
+#include <vector>
 
-TEST(PdControllerTest, Ctor) {
-  EXPECT_NO_THROW({ const auto pd_ctrl = PDController(); });
-}
 
-TEST(PdControllerTest, DISABLED_SetGainsEmpty) {
-  auto pd_ctrl = PDController();
-
-  EXPECT_ANY_THROW(
-      { pd_ctrl.set_gains(Eigen::VectorXd{}, Eigen::VectorXd{}); });
-}
-
-TEST(PdControllerTest, DISABLED_SetGainsWithSpecialDouble) {
-  auto pd_ctrl = PDController();
-
-  constexpr auto size = 3u;
-  auto requested_gains = Gains::Random(size);
-
-  // Test for special double values acceptance or not ?
-  for (const auto &ref : {
-           std::ref(requested_gains.d(0)),
-           // std::ref(requested_gains.d(size - 1)),
-
-           std::ref(requested_gains.p(0)),
-           // std::ref(requested_gains.p(size - 1)),
-       }) {
-    for (auto tmp_value : {
-             std::numeric_limits<double>::infinity(),
-             std::numeric_limits<double>::quiet_NaN(),
-             std::numeric_limits<double>::signaling_NaN(),
-         }) {
-      auto mutation = TemporaryMutate(ref.get(), tmp_value);
-      SCOPED_TRACE(::testing::Message() << '\n' << requested_gains);
-
-      // TODO: Is it an error or not ?
-      EXPECT_ANY_THROW(
-          { pd_ctrl.set_gains(requested_gains.p, requested_gains.d); });
+// Basic fixture
+class PDControllerTest : public ::testing::Test {
+protected:
+    void SetUp() override {
+        controller_ = std::make_unique<linear_feedback_controller::PDController>();
     }
-  }
+
+    std::unique_ptr<linear_feedback_controller::PDController> controller_;
+};
+
+// Verify that controller does not throw exception
+TEST_F(PDControllerTest, ConstructorDoesNotThrow) {
+    ASSERT_NE(controller_, nullptr);
 }
 
-TEST(PdControllerTest, DISABLED_SetGainsWithDifferentSizes) {
-  auto pd_ctrl = PDController();
+// Validate gains configuration with std::vector and effects.
+TEST_F(PDControllerTest, SetGainsWithStdVector) {
+    std::vector<double> p_gains = {10.0, 20.0};
+    std::vector<double> d_gains = {1.0, 2.0};
+    controller_->set_gains(p_gains, d_gains);
 
-  for (auto &&requested_gains :
-       {
-           Gains::Random(1, 2),
-           Gains::Random(4, 3),
-           Gains::Random(50, 1000),
-       })
+    // call compute_control because we does not have getters.
+    Eigen::VectorXd tau_ref(2), q_ref(2), q(2), v(2);
+    tau_ref << 0.0, 0.0;
+    q_ref << 1.0, 1.0;
+    q << 0.5, 0.5;
+    v << 0.1, 0.2;
+    controller_->set_reference(tau_ref, q_ref);
 
-  {
-    SCOPED_TRACE(::testing::Message() << '\n' << requested_gains);
-    EXPECT_ANY_THROW(
-        { pd_ctrl.set_gains(requested_gains.p, requested_gains.d); });
-  }
+    const Eigen::VectorXd& tau = controller_->compute_control(q, v);
+
+    // Kp * (q_ref - q) - Kd * v
+    // tau[0] = 10.0 * (1.0 - 0.5) - 1.0 * 0.1 = 5.0 - 0.1 = 4.9
+    // tau[1] = 20.0 * (1.0 - 0.5) - 2.0 * 0.2 = 10.0 - 0.4 = 9.6
+    EXPECT_NEAR(tau(0), 4.9, 1e-9);
+    EXPECT_NEAR(tau(1), 9.6, 1e-9);
 }
 
-TEST(PdControllerTest, SetGains) {
-  auto pd_ctrl = PDController();
+// Verify error on gain size
+TEST_F(PDControllerTest, SetGainsWithMismatchedSizesThrows) {
+    Eigen::VectorXd p_gains(2);
+    Eigen::VectorXd d_gains(3);
+    EXPECT_THROW(controller_->set_gains(p_gains, d_gains), std::invalid_argument);
 
-  for (auto &&requested_gains :
-       {
-           Gains::Random(1),
-           Gains::Random(3),
-           Gains::Random(4),
-           Gains::Random(50),
-           Gains::Random(1000),
-       })
-
-  {
-    SCOPED_TRACE(::testing::Message() << '\n' << requested_gains);
-    EXPECT_NO_THROW(
-        { pd_ctrl.set_gains(requested_gains.p, requested_gains.d); });
-    // FIXME: How to confirm that it succeeded ?
-  }
+    std::vector<double> p_gains_vec = {1.0, 2.0};
+    std::vector<double> d_gains_vec = {1.0};
+    EXPECT_THROW(controller_->set_gains(p_gains_vec, d_gains_vec), std::invalid_argument);
 }
 
-TEST(PdControllerTest, DISABLED_SetReferencesWithSpecialDouble) {
-  auto pd_ctrl = PDController();
+// Verify error on references size
+TEST_F(PDControllerTest, SetReferenceWithMismatchedSizesThrows) {
+    Eigen::VectorXd tau_ref(2);
+    Eigen::VectorXd q_ref(3);
+    EXPECT_THROW(controller_->set_reference(tau_ref, q_ref), std::invalid_argument);
+}
 
-  constexpr auto size = 3u;
-  auto requested_references = References::Random(size);
+// Verify error on NaN and INF gains values using Eigen
+TEST_F(PDControllerTest, SetGainsWithSpecialValuesThrows) {
+    Eigen::VectorXd p_gains = Eigen::VectorXd::Ones(3);
+    Eigen::VectorXd d_gains = Eigen::VectorXd::Ones(3);
 
-  // Test for special double values acceptance or not ?
-  for (const auto &ref : {
-           std::ref(requested_references.tau(0)),
-           // std::ref(requested_references.tau(size - 1)),
+    double inf = std::numeric_limits<double>::infinity();
+    double nan = std::numeric_limits<double>::quiet_NaN();
 
-           std::ref(requested_references.q(0)),
-           // std::ref(requested_references.q(size - 1)),
-       }) {
-    for (auto tmp_value : {
-             std::numeric_limits<double>::infinity(),
-             std::numeric_limits<double>::quiet_NaN(),
-             std::numeric_limits<double>::signaling_NaN(),
-         }) {
-      auto mutation = TemporaryMutate(ref.get(), tmp_value);
-      SCOPED_TRACE(::testing::Message() << '\n' << requested_references);
+    // Test with NaN
+    p_gains(1) = nan;
+    EXPECT_THROW(controller_->set_gains(p_gains, d_gains), std::invalid_argument);
 
-      // TODO: Is it an error or not ?
-      EXPECT_ANY_THROW({
-        pd_ctrl.set_reference(requested_references.tau, requested_references.q);
-      });
+    // Test with Infinity
+    p_gains(1) = 1.0; // reset
+    d_gains(2) = inf;
+    EXPECT_THROW(controller_->set_gains(p_gains, d_gains), std::invalid_argument);
+}
+
+// Verify error on NaN and INF gains values using vectors
+TEST_F(PDControllerTest, SetGainsFromStdVectorWithSpecialValuesThrows) {
+    std::vector<double> p_gains_vec = {1.0, 1.0, 1.0};
+    std::vector<double> d_gains_vec = {1.0, 1.0, 1.0};
+
+    double inf = std::numeric_limits<double>::infinity();
+    double nan = std::numeric_limits<double>::quiet_NaN();
+
+    // Test with NaN
+    p_gains_vec[1] = nan;
+    EXPECT_THROW(controller_->set_gains(p_gains_vec, d_gains_vec), std::invalid_argument);
+
+    // Test with Infinity
+    p_gains_vec[1] = 1.0; // reset
+    d_gains_vec[2] = inf;
+    EXPECT_THROW(controller_->set_gains(p_gains_vec, d_gains_vec), std::invalid_argument);
+}
+
+// Verify error on NaN and INF references values
+TEST_F(PDControllerTest, SetReferenceWithSpecialValuesThrows) {
+    Eigen::VectorXd q_ref = Eigen::VectorXd::Zero(3);
+    Eigen::VectorXd tau_ref = Eigen::VectorXd::Zero(3);
+
+    double inf = std::numeric_limits<double>::infinity();
+    double nan = std::numeric_limits<double>::quiet_NaN();
+
+    q_ref(0) = nan;
+    EXPECT_THROW(controller_->set_reference(tau_ref, q_ref), std::invalid_argument);
+
+    q_ref(0) = 0.0;
+    tau_ref(1) = inf;
+    EXPECT_THROW(controller_->set_reference(tau_ref, q_ref), std::invalid_argument);
+}
+
+// TODO : empty_vec -> should it be checked ? 
+TEST_F(PDControllerTest, SetGainsWithEmptyVectors) {
+    Eigen::VectorXd empty_vec;
+
+    EXPECT_NO_THROW(controller_->set_gains(empty_vec, empty_vec));
+}
+
+
+// Fixture for computation tests.
+class PDControllerComputationTest : public ::testing::Test {
+protected:
+    void SetUp() override {
+        controller_ = std::make_unique<linear_feedback_controller::PDController>();
+        dof_ = 3;
+
+        p_gains_.resize(dof_);
+        d_gains_.resize(dof_);
+        tau_ref_.resize(dof_);
+        q_ref_.resize(dof_);
+
+        p_gains_ << 10.0, 20.0, 30.0;
+        d_gains_ << 1.0, 2.0, 3.0;
+        tau_ref_ << 5.0, 6.0, 7.0;
+        q_ref_ << 1.0, 1.0, 1.0;
+
+        controller_->set_gains(p_gains_, d_gains_);
+        controller_->set_reference(tau_ref_, q_ref_);
     }
-  }
+
+    std::unique_ptr<linear_feedback_controller::PDController> controller_;
+    int dof_;
+    Eigen::VectorXd p_gains_, d_gains_, tau_ref_, q_ref_;
+};
+
+// Basic computation test
+TEST_F(PDControllerComputationTest, ComputeControlMultiDimensional) {
+    Eigen::VectorXd q(dof_);
+    Eigen::VectorXd v(dof_);
+    q << 0.5, 0.8, 0.9;
+    v << 0.1, -0.2, 0.3;
+
+    const Eigen::VectorXd& tau = controller_->compute_control(q, v);
+
+    // tau = tau_ref + Kp * (q_ref - q) - Kd * v
+    // tau[0] = 5.0 + 10.0 * (1.0 - 0.5) - 1.0 * 0.1   = 5.0 + 5.0 - 0.1 = 9.9
+    // tau[1] = 6.0 + 20.0 * (1.0 - 0.8) - 2.0 * (-0.2) = 6.0 + 4.0 + 0.4 = 10.4
+    // tau[2] = 7.0 + 30.0 * (1.0 - 0.9) - 3.0 * 0.3   = 7.0 + 3.0 - 0.9 = 9.1
+    ASSERT_EQ(tau.size(), dof_);
+    EXPECT_NEAR(tau(0), 9.9, 1e-9);
+    EXPECT_NEAR(tau(1), 10.4, 1e-9);
+    EXPECT_NEAR(tau(2), 9.1, 1e-9);
+
+    // Check that method return only a reference of internal vector.
+    const Eigen::VectorXd& tau2 = controller_->compute_control(q, v);
+    EXPECT_EQ(&tau, &tau2);
 }
 
-TEST(PdControllerTest, DISABLED_SetReferencesWithDifferentSizes) {
-  auto pd_ctrl = PDController();
+// case if no error on q and v.
+TEST_F(PDControllerComputationTest, ComputeControlZeroError) {
+    Eigen::VectorXd q = q_ref_;
+    Eigen::VectorXd v(dof_);
+    v.setZero();
 
-  for (auto &&requested_references :
-       {
-           References::Random(1, 2),
-           References::Random(4, 3),
-           References::Random(50, 1000),
-       })
+    const Eigen::VectorXd& tau = controller_->compute_control(q, v);
 
-  {
-    SCOPED_TRACE(::testing::Message() << '\n' << requested_references);
-    EXPECT_ANY_THROW({
-      pd_ctrl.set_reference(requested_references.tau, requested_references.q);
-    });
-  }
+    // tau = tau_ref + Kp * 0 - Kd * 0 = tau_ref
+    ASSERT_EQ(tau.size(), dof_);
+    for(int i = 0; i < dof_; ++i) {
+        EXPECT_NEAR(tau(i), tau_ref_(i), 1e-9);
+    }
 }
 
-TEST(PdControllerTest, SetReferences) {
-  auto pd_ctrl = PDController();
+// Verify exception on (q, v) size.
+TEST_F(PDControllerComputationTest, ComputeControlWithMismatchedInputSizesThrows) {
+    Eigen::VectorXd q_wrong_size(dof_ - 1);
+    Eigen::VectorXd v_correct_size(dof_);
+    v_correct_size.setZero();
 
-  for (auto &&requested_references :
-       {
-           References::Random(1),
-           References::Random(3),
-           References::Random(4),
-           References::Random(50),
-           References::Random(1000),
-       })
+    Eigen::VectorXd q_correct_size(dof_);
+    Eigen::VectorXd v_wrong_size(dof_ + 1);
+    q_correct_size.setZero();
 
-  {
-    SCOPED_TRACE(::testing::Message() << '\n' << requested_references);
-    EXPECT_NO_THROW({
-      pd_ctrl.set_reference(requested_references.tau, requested_references.q);
-    });
-    // FIXME: How to confirm that it succeeded ?
-  }
-}
-
-TEST(PdControllerTest, ComputeControl) {
-  auto pd_ctrl = PDController();
-
-  for (const auto size : {1u, 2u, 5u, 20u}) {
-    const auto gains = Gains::Random(size);
-    const auto refs = References::Random(size);
-
-    const Eigen::VectorXd arg_q = Eigen::VectorXd::Random(size);
-    const Eigen::VectorXd arg_v = Eigen::VectorXd::Random(size);
-
-    std::stringstream trace_log;
-    trace_log << '\n' << gains << '\n' << refs;
-
-    trace_log << "\nQ = ";
-    PrintTo(arg_q, &trace_log);
-
-    trace_log << "\nV = ";
-    PrintTo(arg_v, &trace_log);
-
-    SCOPED_TRACE(trace_log.str());
-
-    const Eigen::VectorXd expected_control =
-        ExpectedPDControlFrom(gains, refs, arg_q, arg_v);
-
-    SetTo(pd_ctrl, gains);
-    SetTo(pd_ctrl, refs);
-    EXPECT_EQ(pd_ctrl.compute_control(arg_q, arg_v), expected_control);
-
-    // NOTE: compute_control use asserts for errors handling
-    EXPECT_DEBUG_DEATH(
-        {
-          const auto _ = pd_ctrl.compute_control(
-              arg_q, Eigen::VectorXd::Random(arg_q.size() + 1));
-        },
-        ::testing::ContainsRegex("Size missmatch"));
-
-    EXPECT_DEBUG_DEATH(
-        {
-          const auto _ = pd_ctrl.compute_control(
-              Eigen::VectorXd::Random(arg_v.size() + 1), arg_v);
-        },
-        ::testing::ContainsRegex("Size missmatch"));
-
-    EXPECT_DEBUG_DEATH(
-        {
-          const auto _ = pd_ctrl.compute_control(
-              Eigen::VectorXd::Random(refs.q.size() + 1),
-              Eigen::VectorXd::Random(refs.q.size() + 1));
-        },
-        ::testing::ContainsRegex("Size missmatch"));
-  }
+    EXPECT_THROW(controller_->compute_control(q_wrong_size, v_correct_size), std::invalid_argument);
+    EXPECT_THROW(controller_->compute_control(q_correct_size, v_wrong_size), std::invalid_argument);
 }
