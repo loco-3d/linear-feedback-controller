f8c4f67fc62e8dfc23f7d4e1aee987237e7013e7
Clement Pene <cpene@nilgiri>
2025-07-22 09:56:24 +0200

    test(LFController): implement basic tests
    happy path and some robustness tests

--- tests/test_lf_controller.cpp
+++ tests/test_lf_controller.cpp
@@ -1,244 +1,147 @@
-#include <sstream>
-#include <string_view>
-#include <tuple>
-
-#include "utils/mutation.hpp"
-using tests::utils::TemporaryMutate;
-
-#include "utils/lf_controller.hpp"
-using tests::utils::ExpectedLFControlFrom;
-using tests::utils::MakeValidRandomControlFor;
-using tests::utils::MakeValidRandomSensorFor;
-
-#include "utils/robot_model.hpp"
-using tests::utils::JointType;
-using tests::utils::MakeAllModelDescriptionsFor;
-using tests::utils::ModelDescription;
-
-#include "utils/eigen_conversions.hpp"
-using tests::utils::PushNewJointStateTo;
-
 #include "linear_feedback_controller/lf_controller.hpp"
-using linear_feedback_controller::LFController;
-
+#include "utils/mock_robot_model_builder_smart.hpp"
+#include "utils/mock_robot_model_builder.hpp"
+#include "gmock/gmock.h"
 #include "gtest/gtest.h"
 
-using namespace std::literals::string_view_literals;
+using namespace linear_feedback_controller;
 
-namespace {
+// basic fixture "happy path"
+class LFControllerTest : public ::testing::Test {
+protected:
+    std::shared_ptr<SmartMockRobotModelBuilder> mock_robot_builder_;
+    std::unique_ptr<LFController> controller_;
 
-struct LFControllerTest : public ::testing::TestWithParam<ModelDescription> {};
-
-TEST(LFControllerTest, Ctor) {
-  EXPECT_NO_THROW({ auto ctrl = LFController(); });
-}
-
-TEST(LFControllerTest, DISABLED_InitializeNullptr) {
-  auto ctrl = LFController();
-  EXPECT_ANY_THROW({ ctrl.initialize(nullptr); });
-}
-
-TEST(LFControllerTest, DISABLED_InitializeEmptyModel) {
-  auto ctrl = LFController();
-  EXPECT_ANY_THROW({
-    ctrl.initialize(
-        std::make_shared<linear_feedback_controller::RobotModelBuilder>());
-  });
-}
-
-TEST_P(LFControllerTest, Initialize) {
-  const auto model_ptr = std::shared_ptr{MakeRobotModelBuilderFrom(GetParam())};
-  ASSERT_NE(model_ptr, nullptr);
-
-  auto ctrl = LFController();
-  EXPECT_NO_THROW({ ctrl.initialize(model_ptr); });
-}
-
-TEST(LFControllerTest, DISABLED_ComputeControlNotInitialized) {
-  auto ctrl = LFController();
-  EXPECT_ANY_THROW({ auto _ = ctrl.compute_control({}, {}); });
-}
-
-TEST_P(LFControllerTest, DISABLED_ComputeControlNoInput) {
-  const auto model_ptr = std::shared_ptr{MakeRobotModelBuilderFrom(GetParam())};
-  ASSERT_NE(model_ptr, nullptr);
-
-  auto ctrl = LFController();
-  ctrl.initialize(model_ptr);
-
-  EXPECT_ANY_THROW({ auto _ = ctrl.compute_control({}, {}); });
-}
-
-TEST_P(LFControllerTest, DISABLED_ComputeControlUnknownJoints) {
-  const auto model_ptr = std::shared_ptr{MakeRobotModelBuilderFrom(GetParam())};
-  ASSERT_NE(model_ptr, nullptr);
-
-  auto ctrl = LFController();
-  ctrl.initialize(model_ptr);
-
-  const auto sensor = MakeValidRandomSensorFor(*model_ptr);
-  const auto control = MakeValidRandomControlFor(*model_ptr);
-
-  EXPECT_ANY_THROW({
-    auto wrong_sensor = sensor;
-    wrong_sensor.joint_state.name[0] = "this joint doesn't exist";
-    auto _ = ctrl.compute_control(wrong_sensor, control);
-  });
-}
-
-TEST_P(LFControllerTest, DISABLED_ComputeControlSizeMismatch) {
-  const auto model_ptr = std::shared_ptr{MakeRobotModelBuilderFrom(GetParam())};
-  ASSERT_NE(model_ptr, nullptr);
-
-  auto ctrl = LFController();
-  ctrl.initialize(model_ptr);
-
-  const auto sensor = MakeValidRandomSensorFor(*model_ptr);
-  const auto control = MakeValidRandomControlFor(*model_ptr);
-
-  EXPECT_ANY_THROW({
-    auto wrong_sensor = sensor;
-
-    // One more unknown joint inside sensor
-    PushNewJointStateTo(
-        wrong_sensor.joint_state,
-        {.name = "foo", .position = 0.0, .velocity = 0.0, .effort = 0.0});
-
-    auto _ = ctrl.compute_control(wrong_sensor, control);
-  });
-
-  EXPECT_ANY_THROW({
-    auto wrong_control = control;
-
-    // One more unknown joint inside control.initial_state
-    PushNewJointStateTo(
-        wrong_control.initial_state.joint_state,
-        {.name = "foo", .position = 0.0, .velocity = 0.0, .effort = 0.0});
-
-    auto _ = ctrl.compute_control(sensor, wrong_control);
-  });
-
-  EXPECT_ANY_THROW({
-    auto wrong_control = control;
-
-    // One more feed forward term
-    tests::utils::Grow(wrong_control.feedforward, 1);
-    wrong_control.feedforward.tail<1>()[0] = 0.0;
-
-    auto _ = ctrl.compute_control(sensor, wrong_control);
-  });
-
-  EXPECT_ANY_THROW({
-    auto wrong_control = control;
-
-    // One more row/col to feedback gain
-    tests::utils::Grow(wrong_control.feedback_gain, 1);
-
-    wrong_control.feedback_gain.bottomRows<1>() =
-        ::Eigen::VectorXd::Random(wrong_control.feedback_gain.cols());
-
-    wrong_control.feedback_gain.rightCols<1>() =
-        ::Eigen::VectorXd::Random(wrong_control.feedback_gain.rows());
-
-    auto _ = ctrl.compute_control(sensor, wrong_control);
-  });
-}
-
-/// Create a std::tuple<T&, string_view> with the ref expression as string
-#define MakeRefOf(val) std::make_tuple(std::ref((val)), std::string_view{#val})
-
-TEST_P(LFControllerTest, DISABLED_ComputeControlSpecialDouble) {
-  const auto model_ptr = std::shared_ptr{MakeRobotModelBuilderFrom(GetParam())};
-  ASSERT_NE(model_ptr, nullptr);
-
-  auto ctrl = LFController();
-  ctrl.initialize(model_ptr);
-
-  auto sensor = MakeValidRandomSensorFor(*model_ptr);
-  auto control = MakeValidRandomControlFor(*model_ptr);
-
-  // Test for special double values acceptance or not ?
-  for (const auto& [ref, str] : {
-           MakeRefOf(sensor.base_pose(0)),
-           MakeRefOf(sensor.base_pose(3)),
-           MakeRefOf(sensor.base_twist(4)),
-           MakeRefOf(sensor.joint_state.position(0)),
-           MakeRefOf(sensor.joint_state.velocity(0)),
-           MakeRefOf(control.feedforward(0)),
-           MakeRefOf(control.feedback_gain(0, 0)),
-       }) {
-    for (auto tmp_value : {
-             std::numeric_limits<double>::infinity(),
-             std::numeric_limits<double>::quiet_NaN(),
-             std::numeric_limits<double>::signaling_NaN(),
-         }) {
-      const auto mutation = TemporaryMutate(ref, tmp_value);
-      EXPECT_ANY_THROW({ auto _ = ctrl.compute_control(sensor, control); })
-          << str << " = " << tmp_value << " (was " << mutation.OldValue()
-          << ")";
+    void SetUp() override {
+        mock_robot_builder_ = std::make_shared<SmartMockRobotModelBuilder>();
+        controller_ = std::make_unique<LFController>();
+        controller_->initialize(mock_robot_builder_);
     }
-  }
+};
+
+TEST_F(LFControllerTest, CanBeConstructed) {
+    ASSERT_NE(controller_, nullptr);
+    ASSERT_NE(mock_robot_builder_, nullptr);
 }
 
-TEST_P(LFControllerTest, ComputeControl) {
-  const auto model_ptr = std::shared_ptr{MakeRobotModelBuilderFrom(GetParam())};
-  ASSERT_NE(model_ptr, nullptr);
+TEST_F(LFControllerTest, ComputesCorrectControlSignal) {
+    // --- 1. TEST INPUTS CONFIGURATION ---
+    // we use the robotModel inside the smart mocked class (nv = nq = 2)
+    int nq = 2;
+    int nv = 2;
 
-  auto ctrl = LFController();
-  ctrl.initialize(model_ptr);
+    // We define measured and desired states
+    Eigen::VectorXd q_measured(nq); q_measured << 0.1, -0.1;
+    Eigen::VectorXd v_measured(nv); v_measured << 0.2, 0.0;
+    
+    Eigen::VectorXd q_desired(nq); q_desired << 0.0, 0.0;
+    Eigen::VectorXd v_desired(nv); v_desired << 0.0, 0.0;
+    
+    // We define feedforward and gains (these are supposed to be an output of crocoddyl)
+    Eigen::VectorXd feedforward_input(nv);
+    feedforward_input << 9.5, 1.4;
+    Eigen::MatrixXd feedback_gain_input(nv, 2 * nv);
+    // K = [Kp | Kd]
+    // Kp = diag(100, 120)
+    // Kd = diag(20, 25)
+    feedback_gain_input << 100,   0, 20,  0,
+                               0, 120,  0, 25;
 
-  const auto sensor = MakeValidRandomSensorFor(*model_ptr);
-  const auto control = MakeValidRandomControlFor(*model_ptr);
-  EXPECT_EQ(ExpectedLFControlFrom(*model_ptr, sensor, control),
-            ctrl.compute_control(sensor, control));
+    // We put all the generated data on expected format (ROS messages)
+    linear_feedback_controller_msgs::Eigen::Sensor sensor_msg;
+    sensor_msg.joint_state.position = q_measured;
+    sensor_msg.joint_state.velocity = v_measured;
+
+    linear_feedback_controller_msgs::Eigen::Control control_msg;
+    control_msg.initial_state.joint_state.position = q_desired;
+    control_msg.initial_state.joint_state.velocity = v_desired;
+    control_msg.feedforward = feedforward_input;
+    control_msg.feedback_gain = feedback_gain_input;
+
+
+    // --- 2. WE MANUALY CALCULATE EXPECTED RESULT ---
+
+    // Compute error state
+    Eigen::VectorXd diff_state_expected(2 * nv);
+    // Error on position is not a simple soustraction
+    pinocchio::difference(mock_robot_builder_->get_model(), q_measured, q_desired,
+                          diff_state_expected.head(nv));
+
+    // Error on speed is more simple
+    diff_state_expected.tail(nv) = v_desired - v_measured;
+
+    // Compute command law
+    // τ = τ_ff + K * x_err
+    Eigen::VectorXd expected_control(nv);
+    expected_control = feedforward_input + feedback_gain_input * diff_state_expected;
+
+
+    // --- 3. WE CALL THE FUNCTION TO BE TESTED ---
+
+    // First we need to precise how the mock function should react
+    EXPECT_CALL(*mock_robot_builder_, construct_robot_state(testing::_, testing::_, testing::_))
+    .WillOnce(testing::DoAll(
+        testing::SetArgReferee<1>(q_desired),
+        testing::SetArgReferee<2>(v_desired)
+    ))
+    .WillOnce(testing::DoAll(
+        testing::SetArgReferee<1>(q_measured),
+        testing::SetArgReferee<2>(v_measured)
+    ));
+    
+    const Eigen::VectorXd& actual_control = controller_->compute_control(sensor_msg, control_msg);
+
+
+    // --- 4. VERIFICATIONS ---
+
+    // Debug
+    std::cout << "Actual control:   " << actual_control.transpose() << std::endl;
+    std::cout << "Expected control: " << expected_control.transpose() << std::endl;
+
+    ASSERT_TRUE(actual_control.isApprox(expected_control, 1e-9));
 }
 
-constexpr auto dummy_urdf =
-    "<?xml version=\"1.0\" encoding=\"utf-8\"?>"
-    "<robot name=\"dummy\">"
-    "  <link name=\"l0\"/>"
-    "  "
-    "  <joint name=\"l01\" type=\"revolute\">"
-    "    <parent link=\"l0\"/>"
-    "    <child link=\"l1\"/>"
-    "    <origin xyz=\"0 0 1\" rpy=\"0 0 1\"/>"
-    "    <axis xyz=\"0 0 1\"/>"
-    "    <limit lower=\"0\" upper=\"3.14\" velocity=\"100\" effort=\"100\"/>"
-    "  </joint>"
-    "  "
-    "  <link name=\"l1\"/>"
-    "  "
-    "  <joint name=\"l12\" type=\"revolute\">"
-    "    <parent link=\"l1\"/>"
-    "    <child link=\"l2\"/>"
-    "    <origin xyz=\"0 1 0\" rpy=\"1 0 0\"/>"
-    "    <axis xyz=\"0 1 0\"/>"
-    "    <limit lower=\"-3.14\" upper=\"3.14\" velocity=\"100\" effort=\"10\"/>"
-    "  </joint>"
-    "  "
-    "  <link name=\"l2\"/>"
-    "</robot>"sv;
 
-INSTANTIATE_TEST_SUITE_P(
-    DummyUrdf, LFControllerTest,
-    ::testing::ValuesIn(MakeAllModelDescriptionsFor(
-        dummy_urdf,
-        {
-            {
-                {.name = "l01"},
-            },
-            {
-                {.name = "l02", .type = JointType::Controlled},
-            },
-            {
-                {.name = "l01"},
-                {.name = "l12"},
-            },
-        })),
-    [](const auto& info) {
-      std::stringstream stream;
-      PrintTo(info.param, &stream, {.as_param_name = true});
-      return stream.str();
-    });
+// Robustness fixture
+class LFControllerRobustnessTest : public ::testing::Test {
+protected:
+    std::shared_ptr<MockRobotModelBuilder> mock_robot_builder_;
+    std::unique_ptr<LFController> controller_;
 
-}  // namespace
+    void SetUp() override {
+        controller_ = std::make_unique<LFController>();
+    }
+
+};
+
+// initialisation with nullptr model
+TEST_F(LFControllerRobustnessTest, InitializeWithNullModelThrows) {
+    EXPECT_THROW(controller_->initialize(nullptr), std::invalid_argument);
+}
+
+// call of compute_control on a non-initialized controler 
+TEST_F(LFControllerRobustnessTest, ComputeControlThrowsIfNotInitialized) {
+    linear_feedback_controller_msgs::Eigen::Sensor sensor_msg;
+    linear_feedback_controller_msgs::Eigen::Control control_msg;
+
+    EXPECT_THROW(controller_->compute_control(sensor_msg, control_msg), std::runtime_error);
+}
+
+// TEST_F(LFControllerRobustnessTest, ThrowsOnMismatchedMessageSizes) {
+//     // TODO: Implémentez ce test.
+//     mock_robot_builder_ = std::make_shared<SmartMockRobotModelBuilder>();
+//     controller_->initialize(mock_robot_builder_);
+//     // 1. Créez un contrôleur et initialisez-le avec un mock qui attend un DoF de 6.
+//     // 2. Créez un `control_msg` avec `q_ref` de taille 7 (invalide).
+//     // 3. Appelez `compute_control`.
+//     // 4. Utilisez EXPECT_THROW(..., std::invalid_argument) pour vérifier qu'une erreur est levée.
+// }
+
+// TEST_F(LFControllerRobustnessTest, HandlesSpecialFloatValuesInMessages) {
+//     // TODO: Implémentez ce test.
+//     // 1. Créez et initialisez le contrôleur.
+//     // 2. Créez un `sensor_msg` et mettez une valeur NaN ou Infinity dans `v`.
+//     // 3. Appelez `compute_control`.
+//     // 4. Si le contrôleur doit vérifier cela, utilisez EXPECT_THROW. Sinon,
+//     //    ce test peut servir à documenter le comportement actuel.
+// }
